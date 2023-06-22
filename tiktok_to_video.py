1c819c68541cd8d6a30cc934579dfc050da08d8b
Tilek <tilekmaulitbek@gmail.com>
2023-06-22 15:49:10 +0600

    Reworked Main file

--- tiktok_to_video.py
+++ tiktok_to_video.py
@@ -1,11 +1,13 @@
+import os
+
 from pyrogram import Client, filters
 import random
 from TikTokApi import TikTokApi
 
 did = str(random.randint(10000, 999999999))
 app = Client("my_account")
-marat = "mengoyamen"
-delete = True
+
+
 def download(link):
     with TikTokApi(custom_device_id=did) as api:
         video = api.video(url=link)
@@ -14,16 +16,16 @@ def download(link):
             out_file.write(video_data)
 
 
-
-
-@app.on_message(filters.me & filters.chat(marat))
+@app.on_message(filters.me & (filters.chat("mengoyamen") | filters.group))
 def message(client, message):
     if (not message.photo) and ("tiktok.com" in message.text):
         link = message.text
-        app.delete_messages(marat, message.id)
+        app.delete_messages(message.chat.id, message.id)
         print("tiktok found!!")
         download(link)
         print("download completed!!")
-        app.send_video(marat, "out.mp4")
+        app.send_video(message.chat.id, "out.mp4")
+        os.remove('out.mp4')
+
 
 app.run()
